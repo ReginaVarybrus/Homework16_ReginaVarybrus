9568894646ca72661dafb763d1f04a58cb57eac1
Regina Varybrus <varybrusr@gmail.com>
2023-02-11 00:28:49 +0000

    homework done

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// ====== Task1 ======
+console.log('====== Task 1 =====');
 
 class Plane {
     constructor(name, isFlying = false) {
@@ -8,41 +8,113 @@ class Plane {
       this.isFlying = isFlying;
     }
 
-    // // Getter 
-    // get getHeight() {
-    //   return this.height;
-    // }
-
-    // // Setter
-    // set setHeight(newHeight) {
-    //   this.height = newHeight;
-    // }
-
-    // get area() {
-    //   return this.#calcArea();
-    // }
-
     takeOff() {
       return this.isFlying = true;
     }
 
     land() {
-        return this.isFlying = false;
+      return this.isFlying = false;
     }
+}
+
+const plane1 = new Plane('BritishAirlines');
+const plane2 = new Plane('Ryanair');
+const plane3 = new Plane('MAU');
+const plane4 = new Plane('Lufthansa');
+const plane5 = new Plane('TurkishAirlines');
+
+console.log('check plane1', plane1.name, plane1.isFlying);
+console.log('check plane2', plane2.name, plane2.isFlying);
+console.log('check plane3', plane3.name, plane3.isFlying);
+console.log('check plane4', plane4.name, plane4.isFlying);
+console.log('check plane5', plane5.name, plane5.isFlying);
+
+const airport = {
+    planes: [
+        plane1,
+        plane2,
+        plane3,
+        plane4,
+        plane5
+    ],
+    getFlyingPlanes: function() {
+      let planeFlight = this.planes.filter((el) => {
+        return Object.values(el).includes(true);
+      })
+      return planeFlight.length;
+    }
+}
+console.log('check airport', airport);
+console.log('check airport method', airport.getFlyingPlanes());
+
+plane1.takeOff();
+plane3.takeOff();
+
+console.log('plains in the air', airport.getFlyingPlanes());
+
+
+const btn1 = document.getElementById('button1');
+const btn2 = document.getElementById('button2');
+const btn3 = document.getElementById('button3');
+const btn4 = document.getElementById('button4');
+const btn5 = document.getElementById('button5');
+
+btn1.addEventListener('click', () => {
+  if (plane1.isFlying === false) {
+    plane1.takeOff();
   }
+  else {
+    plane1.land();
+  } 
+  console.log(airport.planes);
+  console.log('plains in the air', airport.getFlyingPlanes());
+});
 
-//   const airport = new Plane 
-// Создайте объект airport со свойством planes (массив объектов класса Planes ) и методом getFlyingPlanes(), 
-// который вовзращающий количество элементов из массива planes , у которых isFlying равняется true.
+btn2.addEventListener('click', () => {
+  if (plane2.isFlying === false) {
+    plane2.takeOff();
+  }
+  else {
+    plane2.land();
+  } 
+  console.log(airport.planes);
+  console.log('plains in the air', airport.getFlyingPlanes());
+});
 
-// Cоздайте несколько объектов на основе класса Plane и добавьте их в массив самолетов в airport. 
-// Измените любым объектам значение isFlying на true; Продемонстрируйте метод getFlyingPlanes() в работе.
+btn3.addEventListener('click', () => {
+  if (plane3.isFlying === false) {
+    plane3.takeOff();
+  }
+  else {
+    plane3.land();
+  } 
+  console.log(airport.planes);
+  console.log('plains in the air', airport.getFlyingPlanes());
+});
 
-// Изменение isFlying сделайте при помощи кнопки по нпжптию на которую будет менятся кол-во самолетов в воздухе,
-//  тоесть братся значение getFlyingPlanes
+btn4.addEventListener('click', () => {
+  if (plane4.isFlying === false) {
+    plane4.takeOff();
+  }
+  else {
+    plane4.land();
+  } 
+  console.log(airport.planes);
+  console.log('plains in the air', airport.getFlyingPlanes());
+});
+
+btn5.addEventListener('click', () => {
+  if (plane5.isFlying === false) {
+    plane5.takeOff();
+  }
+  else {
+    plane5.land();
+  } 
+  console.log(airport.planes);
+  console.log('plains in the air', airport.getFlyingPlanes());
+});
 
 console.log('====== Task 2 =====');
-
 
 class User {
     constructor(name, surname) {
@@ -52,11 +124,7 @@ class User {
     get getFullName() {
       return `${this.name} ${this.surname}`;
     }
-  }
-
-const user = new User('Regina', 'Varybrus');
-console.log('check', user.name, user.surname);
-console.log('fullname', user.getFullName);
+}
 
 class Student extends User {
     constructor(name, isFlying, year) {
@@ -72,19 +140,15 @@ class Student extends User {
             console.log('You are not a student!'); // от себя добавила
         }
     }
-  }
+}
+
+const user = new User('Regina', 'Varybrus');
+
+console.log('check user', user.name, user.surname);
+console.log('fullname', user.getFullName);
 
 const student = new Student('Ivan', 'Fomenko', 2020);
-console.log('check', student.name, student.surname, student.year);
+
+console.log('check student', student.name, student.surname, student.year);
 console.log('fullname', student.getFullName);
 console.log('course', student.getCourse);
-
-// 2) Реализуйте класс Student (Студент), который будет наследовать от класса User. 
-// Этот класс должен иметь следующие свойства: name (имя, наследуется от User), surname (фамилия, наследуется от User),
-//  year (год поступления в вуз). Класс должен иметь метод getFullName() (наследуется от User), 
-//  с помощью которого можно вывести одновременно имя и фамилию студента. Также класс должен иметь метод getCourse(), 
-//  который будет выводить текущий курс студента (от 1 до 5). Курс вычисляется так: нужно от текущего года отнять год поступления в вуз. 
-//  Текущий год получите самостоятельно.
-
-// User - имеет только имя и фамилию
-// Student - все остальное
